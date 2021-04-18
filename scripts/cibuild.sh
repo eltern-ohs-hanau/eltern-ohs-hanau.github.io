1cd8a24d0ce06c1941df3b476933bb1b17106478
bitbauer <4582513+bitbauer@users.noreply.github.com>
2021-04-18 23:15:20 +0200

    ADD travis CI

---
+++ scripts/cibuild.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git clone --depth 1 https://github.com/eltern-ohs-hanau/eltern-ohs-hanau.wiki.git _posts/
+revision=$(git -C _posts/ show --pretty=format:"%h" --no-patch)
+
+rm -rf _posts/.git
+bundle exec jekyll build
+git add _posts/
+git commit -m "ADD wiki commit $revision"
+git push
