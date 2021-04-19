b551efa21abc794c3367a5fa6c76a15205813e37
bitbauer <4582513+bitbauer@users.noreply.github.com>
2021-04-19 11:51:45 +0200

    UPDATE travis CI to recommended github pages deployment

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
 rm -rf _posts
 git clone --depth 1 https://github.com/eltern-ohs-hanau/eltern-ohs-hanau.wiki.git _posts/
 revision=$(git -C _posts/ show --pretty=format:"%h" --no-patch)
 rm -rf _posts/.git
 
 bundle exec jekyll build
-git add _posts/
-git commit -m "ADD wiki commit $revision"
-git push
